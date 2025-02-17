d0c1a557aa776cda768e55b2027830a3e0a29910
R0m777 <philipdaoud55@gmail.com>
2025-02-17 16:08:59 +0100

    3rd commit

--- Lezione_01/Hello.py
+++ Lezione_01/Hello.py
@@ -1,4 +1,17 @@
-print("hello World!")
-a=4
-b=5
-print("feffoson")
+
+
+...#esercizio #
+#Memorizza un numero in virgola
+x= (float(input("inserire un numero: ")))
+#Memorizza il risultato
+y = 1.0/ x
+
+print(f"x = {x} ")
+print(f"y = {y} ")
+print(x * y)
+
+
+
+
+
+

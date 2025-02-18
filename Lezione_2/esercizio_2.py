6401e017161bd32db9c6bd4e4bf5d835c1c0cc48
R0m777 <philipdaoud55@gmail.com>
2025-02-18 18:48:50 +0100

    1st commit

---
+++ Lezione_2/esercizio_2.py
@@ -0,0 +1,8 @@
+names =["Michele" , "Chiara" , "Gennaro"]
+print(names[0])
+
+message = ("Come stai?")
+print(f"{names[0]}, {message}\n{names[1]}, {message}\n{names[2]}, {message}")
+
+car_list = ["Lamburghini" , "Ferrari" , "Pagani"]
+ 
---
+++ Lezione_2/esercizio_mario.py
@@ -0,0 +1,13 @@
+n:int =int(input("put a number: "))
+lista1 = ["1st" , "2nd" , "3rd"] 
+if n == 1:
+    print("1st")
+elif n == 2:
+    print(lista1[1])
+elif n==3:
+    print(lista1[2])
+else:
+    print(f"{n}th")
+
+
+

16f12917eca15ab430ef95bae044b3905594ff5c
R0m777 <philipdaoud55@gmail.com>
2025-02-19 18:21:44 +0100

    1st commit

---
+++ Lezione_01/esercizi.py
@@ -0,0 +1,5 @@
+ITS_menu:dict = {"pizza": 9.00 , "Pasta": 12.00 , "Cotoletta": 15.20}
+Totale = 0
+Totale = Totale + ITS_menu["pizza"]
+Totale = Totale + ITS_menu["Cotoletta"]
+print(f"il tuo totale è: {Totale:.2f}")   

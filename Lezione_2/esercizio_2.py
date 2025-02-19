be9812949d00bb079381209e93b5fd1fdeda596f
R0m777 <philipdaoud55@gmail.com>
2025-02-19 19:23:00 +0100

    3rd commit

--- Lezione_2/esercizio_2.py
+++ Lezione_2/esercizio_2.py
@@ -6,4 +6,23 @@ print(f"{names[0]}, {message}\n{names[1]}, {message}\n{names[2]}, {message}")
 
 car_list = ["Lamburghini" , "Ferrari" , "Pagani"]
 print(f"La {car_list[1]} è superiore alla {car_list[0] }.")
- 
+
+print(f"{names[2]} non puo venire.")
+names.pop(2) 
+names.append("Gattuso")
+print(f"{names[0]}, {message}\n{names[1]}, {message}\n{names[2]}, {message}")
+
+names2 = ["Del piero" , "Ronaldo" , "Antonella"]
+names.insert(0, "Mario")
+names.insert(2, "Flavia")
+print(names)
+names.extend(names2)
+print(names)
+for name in names:
+    print(f"{name} {message}")
+
+print(f"{names[4]} non sei invitato")   
+names.pop(4)
+del names[4:]
+print(names) 
+

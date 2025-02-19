8af00fcd370f142e5d7f1cae5a44c78f414bfba1
R0m777 <philipdaoud55@gmail.com>
2025-02-19 18:23:16 +0100

    2nd commit

--- Lezione_01/Hello.py
+++ Lezione_01/Hello.py
@@ -10,6 +10,11 @@ print(f"x = {x} ")
 print(f"y = {y} ")
 print(x * y)
 
+n = 2.3
+y = n%2.0
+print(f"n = {n} , y = {y:2f}" )
+
+
 
 
 
--- Lezione_2/esercizio_2.py
+++ Lezione_2/esercizio_2.py
@@ -1,8 +1,9 @@
 names =["Michele" , "Chiara" , "Gennaro"]
 print(names[0])
 
-message = ("Come stai?")
+message = ("Ti va di cenare da me?")
 print(f"{names[0]}, {message}\n{names[1]}, {message}\n{names[2]}, {message}")
 
 car_list = ["Lamburghini" , "Ferrari" , "Pagani"]
+print(f"La {car_list[1]} è superiore alla {car_list[0] }.")
  

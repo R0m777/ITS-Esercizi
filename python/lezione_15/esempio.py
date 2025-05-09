fe17aed4cddfa23ba782b23cdf4501d8b38a3c32
R0m777 <philipdaoud55@gmail.com>
2025-05-09 13:29:42 +0200

    2nd

--- python/lezione_15/esempio.py
+++ python/lezione_15/esempio.py
@@ -9,5 +9,38 @@ try:
 finally:
     file.close()'''
 
-with open("lezione_15/example.txt", "w") as file:
-    print(file.read())
+'''with open("lezione_15/example.txt", "w") as file:
+    print(file.read())'''
+
+'''class MyResources:
+
+    def __enter__(self):
+        print("Sono nella funzione __enter__")
+        return self
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        print("Sono nella funzione __exit__")
+
+        if exc_type is not None:
+            print(f"Error: {exc_value}")
+        return True
+    
+print("Inizio programma")
+
+with MyResources() as rescources:
+    print("sono dentro il blocco with")
+
+
+with open("lezione_15/example.txt") as reader:
+    print(reader.read())'''
+
+import json
+
+file = open("lezione_15/config.json","w")
+
+db: dict = {"gdhdhd": {"ciao":"mincia", "age": 30},
+            "hjdjksahj": {"giovanni": "name"}}
+
+json.dump(db, file)
+
+file.close()

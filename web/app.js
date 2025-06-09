f8c94f3d24294530f4a51f482804a0e3fe4beb56
R0m777 <philipdaoud55@gmail.com>
2025-06-09 19:15:18 +0200

    1st

---
+++ web/app.js
@@ -0,0 +1,58 @@
+import * as util from "./util.js"
+
+console.log("Funzia")
+
+for(let i=0;i<4;i++){
+    console.log(i)
+}
+
+
+
+const arr1=[1,2,3];
+let [a,b]=arr1;
+console.log(a,b);
+
+console.log(arr1,...arr1);
+
+const arr2=[...arr1];
+
+const arr3=[...arr1,...arr2];
+console.log(arr3)
+
+arr1.push(4);
+arr2.unshift(0);
+console.log(arr1,arr2);
+
+const prof={
+    nome:"Rob",
+    cognome:"Del",
+    età:48,
+    indirizzo:{
+        via:"via cesare",
+        città:"Roma"
+    }
+}
+
+let {nome:nomi,cognome}=prof
+console.log(nomi,cognome)
+
+console.log(prof.indirizzo.via);
+console.log(prof["cognome"]);
+
+const prof2=new Object();
+
+prof2.nome="Rob";
+prof2.cognome="Del";
+console.log(prof2)
+
+function persona(nome="", cognome=""){
+    this.nome=cognome;
+    this.cognome=nome;
+}
+
+const roberto=new persona("Rob","Del");
+const mario=new persona("Mario","Rossi");
+
+mario.telefono="1234";
+
+console.log(roberto,mario);

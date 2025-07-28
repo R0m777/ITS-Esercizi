393ce530d29e77cbf4ce54f2ecc70461975f3109
R0m777 <philipdaoud55@gmail.com>
2025-07-28 11:35:56 +0200

    1st

---
+++ web/seconda-app/src/UserAlbum.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const UserAlbum = () => {
+    //fetch da https://jsonplaceholder.typicode.com/albums
+    // useState users
+    //useState userSelected
+  return (
+    <div className='container'>
+        <div className='row'>
+            <div className='col-6'>
+                <select>
+                    <option value="">Seleziona Utente</option>
+                </select>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default UserAlbum
